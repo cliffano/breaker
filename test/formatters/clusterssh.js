521bd0e9f3a77d2054cfd1393a0ef929f10d3e9d
Cliffano Subagio <cliffano@gmail.com>
2012-11-04 16:34:08 +1100

    Impl test placeholders.

--- test/formatters/clusterssh.js
+++ test/formatters/clusterssh.js
@@ -10,5 +10,38 @@ describe('clusterssh', function () {
     });
   });
 
+  describe('format', function () {
+
+    it('should return empty string when conf does not have any host', function () {
+      formatter.format([]).should.equal('');
+    });
+
+    it('should format lines with clusters header', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] }
+      ];
+      formatter.format(conf).should.equal('clusters = dev1\n\ndev1 = dev1.com');
+    });
+
+    it('should group hosts by label', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] },
+        { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'shared' ] },
+        { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'shared' ] }
+      ];
+      formatter.format(conf).should.equal('clusters = dev1 shared\n\ndev1 = dev1.com\nshared = dev2.com dev3.com');
+    });
+
+    it('should sort by host when there are multiple hosts', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'ccc' ] },
+        { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'aaa' ] },
+        { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'bbb' ] }
+      ];
+      formatter.format(conf).should.equal('clusters = aaa bbb ccc\n\naaa = dev2.com\nbbb = dev3.com\nccc = dev1.com');
+    });
+
+  });
+
 });
  
--- test/formatters/sshconfig.js
+++ test/formatters/sshconfig.js
@@ -10,5 +10,40 @@ describe('sshconfig', function () {
     });
   });
 
+  describe('format', function () {
+
+    it('should return empty string when conf does not have any host', function () {
+      formatter.format([]).should.equal('');
+    });
+
+    it('should format config paragraph when conf only has a single host', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] }
+      ];
+      formatter.format(conf).should.equal('Host dev1\n    HostName dev1.com\n    User user1');
+    });
+
+    it('should not include host which label is used by multiple hosts', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] },
+        { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'shared' ] },
+        { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'shared' ] }
+      ];
+      formatter.format(conf).should.equal('Host dev1\n    HostName dev1.com\n    User user1');
+    });
+
+    it('should sort by label when there are multiple config paragraphs', function () {
+      var conf = [
+        { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'ccc' ] },
+        { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'aaa' ] },
+        { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'bbb' ] }
+      ];
+      formatter.format(conf).should.equal(
+        'Host aaa\n    HostName dev2.com\n    User user2\n\n' +
+        'Host bbb\n    HostName dev3.com\n    User user3\n\n' +
+        'Host ccc\n    HostName dev1.com\n    User user1'
+      );
+    });
+  });
 });
  

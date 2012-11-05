553e129ed3161ee801aa1564cc9a16981951e3a1
Cliffano Subagio <cliffano@gmail.com>
2012-11-06 01:06:48 +1100

    Implement Breaker#ssh for executing remote command.

--- test/breaker.js
+++ test/breaker.js
@@ -56,7 +56,7 @@ describe('breaker', function () {
           cli: {
             readConfigFileSync: function (file) {
               checks.cli_file = file;
-              return '{ "foo": "bar" }';
+              return '[{ "foo": "bar" }]';
             }
           }
         },
@@ -71,11 +71,104 @@ describe('breaker', function () {
       breaker.format('sshboom', function () {
         done();
       }); 
-      checks.format_conf.foo.should.equal('bar');
+      checks.format_conf[0].foo.should.equal('bar');
       checks.console_log_messages.length.should.equal(1);
       checks.console_log_messages[0].should.equal('some formatted output');
     }); 
   });
 
+  describe('ssh', function () {
+
+    it('should exec ssh command to all hosts when labels are not supplied', function (done) {
+      checks.exec_commands = [];
+      mocks.requires = {
+        'bagofholding': {
+          cli: {
+            readConfigFileSync: function (file) {
+              checks.cli_file = file;
+              return '[' +
+                '{ "host": "dev1.json", "port": 22, "user": "user1", "key": "id_rsa1", "labels": "dev1" },' +
+                '{ "host": "dev2.json", "port": 22, "user": "user2", "key": "id_rsa2", "labels": "dev2" },' +
+                '{ "host": "dev3.json", "port": 22, "user": "user3", "key": "id_rsa3", "labels": "dev3" }' +
+                ']';
+            },
+            exec: function (command, fallthrough, cb) {
+              fallthrough.should.equal(true);
+              checks.exec_commands.push(command);
+              cb();
+            }
+          }
+        }
+      };
+      breaker = new (create(checks, mocks))();
+      breaker.ssh('df -kh;', undefined, function () {
+        done();
+      }); 
+      checks.console_log_messages.length.should.equal(3);
+      checks.console_log_messages[0].should.equal('+ dev1.json');
+      checks.console_log_messages[1].should.equal('+ dev2.json');
+      checks.console_log_messages[2].should.equal('+ dev3.json');
+      checks.exec_commands.length.should.equal(3);
+      checks.exec_commands[0].should.equal('ssh -i id_rsa1 user1@dev1.json:22 \'df -kh;\'');
+      checks.exec_commands[1].should.equal('ssh -i id_rsa2 user2@dev2.json:22 \'df -kh;\'');
+      checks.exec_commands[2].should.equal('ssh -i id_rsa3 user3@dev3.json:22 \'df -kh;\'');
+    }); 
+
+    it('should construct command without key, user, and port when they are not configured', function (done) {
+      checks.exec_commands = [];
+      mocks.requires = {
+        'bagofholding': {
+          cli: {
+            readConfigFileSync: function (file) {
+              checks.cli_file = file;
+              return '[' +
+                '{ "host": "dev1.json", "labels": "dev1" }' +
+                ']';
+            },
+            exec: function (command, fallthrough, cb) {
+              fallthrough.should.equal(true);
+              checks.exec_commands.push(command);
+              cb();
+            }
+          }
+        }
+      };
+      breaker = new (create(checks, mocks))();
+      breaker.ssh('df -kh;', undefined, function () {
+        done();
+      }); 
+      checks.console_log_messages.length.should.equal(1);
+      checks.console_log_messages[0].should.equal('+ dev1.json');
+      checks.exec_commands.length.should.equal(1);
+      checks.exec_commands[0].should.equal('ssh  dev1.json \'df -kh;\'');
+    }); 
+
+    it('should not execute any command when labels opt do not match any label in configuration', function (done) {
+      checks.exec_commands = [];
+      mocks.requires = {
+        'bagofholding': {
+          cli: {
+            readConfigFileSync: function (file) {
+              checks.cli_file = file;
+              return '[' +
+                '{ "host": "dev1.json", "labels": "dev1" }' +
+                ']';
+            },
+            exec: function (command, fallthrough, cb) {
+              fallthrough.should.equal(true);
+              checks.exec_commands.push(command);
+              cb();
+            }
+          }
+        }
+      };
+      breaker = new (create(checks, mocks))();
+      breaker.ssh('df -kh;', ['foo1', 'foo2', 'foo3'], function () {
+        done();
+      }); 
+      checks.console_log_messages.length.should.equal(0);
+      checks.exec_commands.length.should.equal(0);
+    });
+  });
 });
  
--- test/cli.js
+++ test/cli.js
@@ -24,8 +24,13 @@ describe('cli', function () {
               checks.breaker_init_exit = exit;
             },
             format: function (type, exit) {
-              checks.breaker_gen_type = type;
-              checks.breaker_gen_exit = exit;
+              checks.breaker_format_type = type;
+              checks.breaker_format_exit = exit;
+            },
+            ssh: function (command, labels, exit) {
+              checks.breaker_ssh_command = command;
+              checks.breaker_ssh_labels = labels;
+              checks.breaker_ssh_exit = exit;
             }
           };
         }
@@ -58,17 +63,33 @@ describe('cli', function () {
       checks.bag_parse_commands.format.options[0].arg.should.equal('-t, --type <type>');
       checks.bag_parse_commands.format.options[0].desc.should.equal('Format type');
       checks.bag_parse_commands.format.action({});
-      checks.breaker_gen_type.should.equal('sshconfig');
-      checks.breaker_gen_exit.should.be.a('function');
+      checks.breaker_format_type.should.equal('sshconfig');
+      checks.breaker_format_exit.should.be.a('function');
     });
 
     it('should contain format command with type when args contain specified type', function () {
       checks.bag_parse_commands.format.desc.should.equal('Format hosts info into specific type');
       checks.bag_parse_commands.format.action({ type: 'clusterssh' });
-      checks.breaker_gen_type.should.equal('clusterssh');
-      checks.breaker_gen_exit.should.be.a('function');
+      checks.breaker_format_type.should.equal('clusterssh');
+      checks.breaker_format_exit.should.be.a('function');
     });
 
+    it('should contain ssh command and delegate to breaker ssh when exec is called', function () {
+      checks.bag_parse_commands.ssh.desc.should.equal('Remotely execute shell command on selected configured hosts in parallel via SSH');
+      checks.bag_parse_commands.ssh.options[0].arg.should.equal('-l, --labels <labels>');
+      checks.bag_parse_commands.ssh.options[0].desc.should.equal('Comma separated labels');
+      checks.bag_parse_commands.ssh.action('df -kh;', { labels: 'foo,bar' });
+      checks.breaker_ssh_command.should.equal('df -kh;');
+      checks.breaker_ssh_labels.length.should.equal(2);
+      checks.breaker_ssh_labels[0].should.equal('foo');
+      checks.breaker_ssh_labels[1].should.equal('bar');
+      checks.breaker_ssh_exit.should.be.a('function');
+    });
+
+    it('should pass empty array labels when labels flag is not specified', function () {
+      checks.bag_parse_commands.ssh.action('df -kh;', {});
+      checks.breaker_ssh_labels.length.should.equal(0);
+    });
   });
 });
  

794417d154d2d2f89cfd81b854e60fa19f824e67
Cliffano Subagio <cliffano@gmail.com>
2013-03-02 15:25:43 +1100

    Fix sshconfig format handling of latest breaker config file.

--- test/formatters/sshconfig.js
+++ test/formatters/sshconfig.js
@@ -9,28 +9,28 @@ buster.testCase('sshconfig - format', {
   },
   'should format config paragraph when conf only has a single host': function () {
     var conf = [
-      { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] }
+      { host: 'dev1.com', ssh: [{ user: 'user1', port: 2222, key: 'path/to/key1' }], labels: [ 'dev1' ] }
     ];
-    assert.equals(formatter.format(conf), 'Host dev1\n    HostName dev1.com\n    User user1');
+    assert.equals(formatter.format(conf), 'Host dev1\n    HostName dev1.com\n    IdentityFile path/to/key1\n    User user1\n    Port 2222\n\n');
   },
   'should not include host which label is used by multiple hosts': function () {
     var conf = [
-      { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] },
-      { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'shared' ] },
-      { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'shared' ] }
+      { host: 'dev1.com', ssh: [{ user: 'user1', key: 'path/to/key1' }], labels: [ 'dev1' ] },
+      { host: 'dev2.com', ssh: [{ user: 'user2', key: 'path/to/key2' }], labels: [ 'shared' ] },
+      { host: 'dev3.com', ssh: [{ user: 'user3', key: 'path/to/key3' }], labels: [ 'shared' ] }
     ];
-    assert.equals(formatter.format(conf), 'Host dev1\n    HostName dev1.com\n    User user1');
+    assert.equals(formatter.format(conf), 'Host dev1\n    HostName dev1.com\n    IdentityFile path/to/key1\n    User user1\n\n');
   },
   'should sort by label when there are multiple config paragraphs': function () {
     var conf = [
-      { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'ccc' ] },
-      { host: 'dev2.com', desc: 'dev2desc', user: 'user2', key: 'path/to/key2', labels: [ 'aaa' ] },
-      { host: 'dev3.com', desc: 'dev3desc', user: 'user3', key: 'path/to/key3', labels: [ 'bbb' ] }
+      { host: 'dev1.com', ssh: [{ user: 'user1' }], labels: [ 'ccc' ] },
+      { host: 'dev2.com', ssh: [{ user: 'user2' }], labels: [ 'aaa' ] },
+      { host: 'dev3.com', ssh: [{ user: 'user3' }], labels: [ 'bbb' ] }
     ];
     assert.equals(formatter.format(conf), 
-      'Host aaa\n    HostName dev2.com\n    User user2\n\n' +
-      'Host bbb\n    HostName dev3.com\n    User user3\n\n' +
-      'Host ccc\n    HostName dev1.com\n    User user1'
+      'Host aaa\n    HostName dev2.com\n    User user2\n\n\n' +
+      'Host bbb\n    HostName dev3.com\n    User user3\n\n\n' +
+      'Host ccc\n    HostName dev1.com\n    User user1\n\n'
     );
   }
 });

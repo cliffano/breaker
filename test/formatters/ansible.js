94ebdfc99424901c5634458794d1e5dc8d2f0e9c
Cliffano Subagio <cliffano@gmail.com>
2013-03-02 14:11:18 +1100

    Add ansible type for formatting Ansible inventory file.

---
+++ test/formatters/ansible.js
@@ -0,0 +1,40 @@
+var bag = require('bagofholding'),
+  buster = require('buster'),
+  formatter = require('../../lib/formatters/ansible');
+
+buster.testCase('ansible - format', {
+  'should return empty string when conf does not have any host': function () {
+    var conf = [];
+    assert.equals(formatter.format(conf), '');
+  },
+  'should format lines with clusters header': function () {
+    var conf = [
+      { host: 'dev1.com', desc: 'dev1desc', user: 'user1', key: 'path/to/key1', labels: [ 'dev1' ] }
+    ];
+    assert.equals(formatter.format(conf), '[dev1]\ndev1.com\n');
+  },
+  'should group hosts by label': function () {
+    var conf = [
+      { host: 'dev1.com', desc: 'dev1desc', ssh: { user: 'user1', key: 'path/to/key1' }, labels: [ 'dev1' ] },
+      { host: 'dev2.com', desc: 'dev2desc', ssh: { user: 'user2', key: 'path/to/key2' }, labels: [ 'shared' ] },
+      { host: 'dev3.com', desc: 'dev3desc', ssh: { user: 'user3', key: 'path/to/key3' }, labels: [ 'shared' ] }
+    ];
+    assert.equals(formatter.format(conf), '[dev1]\ndev1.com\n\n[shared]\ndev2.com\ndev3.com\n');
+  },
+  'should sort by host when there are multiple hosts': function () {
+    var conf = [
+      { host: 'dev1.com', desc: 'dev1desc', ssh: { user: 'user1', key: 'path/to/key1' }, labels: [ 'ccc' ] },
+      { host: 'dev2.com', desc: 'dev2desc', ssh: { user: 'user2', key: 'path/to/key2' }, labels: [ 'aaa' ] },
+      { host: 'dev3.com', desc: 'dev3desc', ssh: { user: 'user3', key: 'path/to/key3' }, labels: [ 'bbb' ] }
+    ];
+    assert.equals(formatter.format(conf), '[aaa]\ndev2.com\n\n[bbb]\ndev3.com\n\n[ccc]\ndev1.com\n');
+  },
+  'should exclude non-ssh host': function () {
+    var conf = [
+      { host: 'dev1.com', desc: 'dev1desc', ssh: { user: 'user1', key: 'path/to/key1' }, labels: [ 'ccc' ] },
+      { host: 'dev2.com', desc: 'dev2desc', labels: [ 'aaa' ] },
+      { host: 'dev3.com', desc: 'dev3desc', ssh: { user: 'user3', key: 'path/to/key3' }, labels: [ 'bbb' ] }
+    ];
+    assert.equals(formatter.format(conf), '[aaa]\ndev2.com\n\n[bbb]\ndev3.com\n\n[ccc]\ndev1.com\n');
+  }
+});
